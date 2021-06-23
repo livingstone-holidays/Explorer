fef2ca8744f25a55192f78621578c1182a5a3a75
livingstone-holidays <86318525+livingstone-holidays@users.noreply.github.com>
2021-06-23 23:01:17 +0530

    Update config.js

--- config.js
+++ config.js
@@ -2,14 +2,7 @@ const config = {
   style: "mapbox://styles/mapbox/light-v10",
   accessToken:
     "pk.eyJ1IjoibGl2aW5nc3RvbmVzdGF5cyIsImEiOiJja3EyOGZoNTEwbTJ0MndwODVvdmlsMzNpIn0.1RIytc5O3-uTZHCjysTAuw",
-CSV= $(document).ready(function () {
-      $.ajax({
-        type: "GET",
-        //YOUR TURN: Replace with csv export link
-        url: 'https://docs.google.com/spreadsheets/d/1n8vdoGi86H6-3dgfL9zI2L_N6n2i9VzJkvpSP9LRWV4/gviz/tq?tqx=out:csv&sheet=MapboxData',
-        dataType: "text",
-        success: function (csvData) { makeGeoJSON(csvData); }
-      });,
+CSV: "https://docs.google.com/spreadsheets/d/1n8vdoGi86H6-3dgfL9zI2L_N6n2i9VzJkvpSP9LRWV4/export?format=csv&id=1n8vdoGi86H6-3dgfL9zI2L_N6n2i9VzJkvpSP9LRWV4&gid=0",
   center: [-120.234, 47.398],
   zoom: 6,
   title: "Replace with your title",
