e97af192e535b5d6947c326184fa2640c6c6ea13
livingstone-holidays <86318525+livingstone-holidays@users.noreply.github.com>
2021-06-25 12:10:31 +0530

    Update app.js

--- app.js
+++ app.js
@@ -29,10 +29,14 @@ function createPopup(currentFeature) {
   if (popups[0]) popups[0].remove();
   const popup = new mapboxgl.Popup({ closeOnClick: true })
     .setLngLat(currentFeature.geometry.coordinates)
-         .setHTML("<div color='#cfc'><h3>" + currentFeature.properties[config.popupInfo[0]] 
-         + "</h3></div><h4><b>Region: </b>" + currentFeature.properties[config.popupInfo[1]] 
-         + "</h4><h4><b>Property Type: </b>" + currentFeature.properties[config.popupInfo[2]] 
-         + "</h4>")    .addTo(map);
+     .setHTML("<table><tr><th style='background-color:#F2E74D'>" 
+              + currentFeature.properties[config.popupInfo[0]]
+              + "</th></tr><tr><td><h4><b>Region: </b>" 
+              + currentFeature.properties[config.popupInfo[1]] 
+              + "</h4></td></tr><tr><td><h4><b>Property Type: </b>" 
+              + currentFeature.properties[config.popupInfo[2]] 
+              + "</h4></td></tr></table>")
+  .addTo(map);
 }
 
 function buildLocationList(locationData) {
