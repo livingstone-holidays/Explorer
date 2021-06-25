bc1dc545196510b173efa0815a9713c33b8b2609
livingstone-holidays <86318525+livingstone-holidays@users.noreply.github.com>
2021-06-25 13:00:09 +0530

    Update app.js

--- app.js
+++ app.js
@@ -457,10 +457,10 @@ map.on("load", function () {
           },
           paint: {
             "circle-radius": 5, // size of circles
-            "circle-color": "#3D2E5D", // color of circles
+            "circle-color": "#d6461a", // color of circles
             "circle-stroke-color": "white",
             "circle-stroke-width": 1,
-            "circle-opacity": 0.8,
+            "circle-opacity": 1,
           },
         });
       }
